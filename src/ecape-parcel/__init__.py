3b101307787d262e149a333785a86b29db240c64
a-urq <amelia.r.urquhart@gmail.com>
2023-12-25 23:41:27 -0600

    Added parcel paths and made sure they work using a SounderPy test.

--- src/ecape-parcel/__init__.py
+++ src/ecape-parcel/__init__.py
@@ -1,2 +1,12 @@
-import ecape as ecape
+import metpy as mpy
+import metpy.calc as mpcalc
+import pint
+import math
+import numpy as np
+import sys
 
+from ecape.calc import calc_ecape, calc_el_height
+from metpy.units import check_units, units
+from pint import UnitRegistry
+
+from typing import Tuple
