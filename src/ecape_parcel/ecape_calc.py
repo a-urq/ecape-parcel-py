Fixed erroneous computation of psi parameter in ecape_calc.py

src/ecape_parcel/ecape_calc.py
@@ -6,6 +6,9 @@
 # There are also a few issues related to MetPy weirdness and irreversible adiabatic descent that
 # I've worked out here.
 
+# The calculation for the Psi parameter appeared to have an extra 4.0 coefficient in the denominator
+# that didn't appear in Peters et. al. 2023 Eq. 52, so I've removed it here
+
 # SPDX-FileCopyrightText: 2023-present Robert Capella <bob.capella@gmail.com>
 # SPDX-License-Identifier: MIT
 
@@ -338,7 +341,7 @@ def calc_psi(el_z: pint.Quantity) -> pint.Quantity:
     pr = (1.0 / 3.0) * units("dimensionless")  # prandtl number
     ksq = 0.18 * units("dimensionless")  # von karman constant
 
-    psi = (ksq * alpha**2 * np.pi**2 * l_mix) / (4.0 * pr * sigma**2 * el_z)
+    psi = (ksq * alpha**2 * np.pi**2 * l_mix) / (pr * sigma**2 * el_z)
 
     return psi
 